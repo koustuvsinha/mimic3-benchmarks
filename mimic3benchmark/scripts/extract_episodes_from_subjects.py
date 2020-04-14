7059d3577cbcf033ab03b78ac635f5e093211ef4
Koustuv Sinha <koustuvsinha@hotmail.com>
2020-04-14 07:00:29 -0700

    add script to extract new variables, add analysis notebooks

--- mimic3benchmark/scripts/extract_episodes_from_subjects.py
+++ mimic3benchmark/scripts/extract_episodes_from_subjects.py
@@ -6,34 +6,61 @@ import os
 import sys
 from tqdm import tqdm
 
-from mimic3benchmark.subject import read_stays, read_diagnoses, read_events, get_events_for_stay,\
-    add_hours_elpased_to_events
-from mimic3benchmark.subject import convert_events_to_timeseries, get_first_valid_from_timeseries
-from mimic3benchmark.preprocessing import read_itemid_to_variable_map, map_itemids_to_variables, clean_events
+from mimic3benchmark.subject import (
+    read_stays,
+    read_diagnoses,
+    read_events,
+    get_events_for_stay,
+    add_hours_elpased_to_events,
+)
+from mimic3benchmark.subject import (
+    convert_events_to_timeseries,
+    get_first_valid_from_timeseries,
+)
+from mimic3benchmark.preprocessing import (
+    read_itemid_to_variable_map,
+    map_itemids_to_variables,
+    clean_events,
+)
 from mimic3benchmark.preprocessing import assemble_episodic_data
 
+import ray
 
-parser = argparse.ArgumentParser(description='Extract episodes from per-subject data.')
-parser.add_argument('subjects_root_path', type=str, help='Directory containing subject sub-directories.')
-parser.add_argument('--variable_map_file', type=str,
-                    default=os.path.join(os.path.dirname(__file__), '../resources/itemid_to_variable_map.csv'),
-                    help='CSV containing ITEMID-to-VARIABLE map.')
-parser.add_argument('--reference_range_file', type=str,
-                    default=os.path.join(os.path.dirname(__file__), '../resources/variable_ranges.csv'),
-                    help='CSV containing reference ranges for VARIABLEs.')
+ray.init()
+
+
+parser = argparse.ArgumentParser(description="Extract episodes from per-subject data.")
+parser.add_argument(
+    "subjects_root_path", type=str, help="Directory containing subject sub-directories."
+)
+parser.add_argument(
+    "--variable_map_file",
+    type=str,
+    default=os.path.join(
+        os.path.dirname(__file__), "../resources/itemid_to_variable_map.csv"
+    ),
+    help="CSV containing ITEMID-to-VARIABLE map.",
+)
+parser.add_argument(
+    "--reference_range_file",
+    type=str,
+    default=os.path.join(os.path.dirname(__file__), "../resources/variable_ranges.csv"),
+    help="CSV containing reference ranges for VARIABLEs.",
+)
 args, _ = parser.parse_known_args()
 
-var_map = read_itemid_to_variable_map(args.variable_map_file)
-variables = var_map.VARIABLE.unique()
 
-for subject_dir in tqdm(os.listdir(args.subjects_root_path), desc='Iterating over subjects'):
+@ray.remote
+def extract(subject_dir, args):
+    var_map = read_itemid_to_variable_map(args.variable_map_file)
+    variables = var_map.VARIABLE.unique()
     dn = os.path.join(args.subjects_root_path, subject_dir)
     try:
         subject_id = int(subject_dir)
         if not os.path.isdir(dn):
             raise Exception
     except:
-        continue
+        return
 
     try:
         # reading tables of this subject
@@ -41,8 +68,8 @@ for subject_dir in tqdm(os.listdir(args.subjects_root_path), desc='Iterating ove
         diagnoses = read_diagnoses(os.path.join(args.subjects_root_path, subject_dir))
         events = read_events(os.path.join(args.subjects_root_path, subject_dir))
     except:
-        sys.stderr.write('Error reading from disk for subject: {}\n'.format(subject_id))
-        continue
+        sys.stderr.write("Error reading from disk for subject: {}\n".format(subject_id))
+        return
 
     episodic_data = assemble_episodic_data(stays, diagnoses)
 
@@ -51,7 +78,7 @@ for subject_dir in tqdm(os.listdir(args.subjects_root_path), desc='Iterating ove
     events = clean_events(events)
     if events.shape[0] == 0:
         # no valid events for this subject
-        continue
+        return
     timeseries = convert_events_to_timeseries(events, variables=variables)
 
     # extracting separate episodes
@@ -65,15 +92,49 @@ for subject_dir in tqdm(os.listdir(args.subjects_root_path), desc='Iterating ove
             # no data for this episode
             continue
 
-        episode = add_hours_elpased_to_events(episode, intime).set_index('HOURS').sort_index(axis=0)
+        episode = (
+            add_hours_elpased_to_events(episode, intime)
+            .set_index("HOURS")
+            .sort_index(axis=0)
+        )
         if stay_id in episodic_data.index:
-            episodic_data.loc[stay_id, 'Weight'] = get_first_valid_from_timeseries(episode, 'Weight')
-            episodic_data.loc[stay_id, 'Height'] = get_first_valid_from_timeseries(episode, 'Height')
-        episodic_data.loc[episodic_data.index == stay_id].to_csv(os.path.join(args.subjects_root_path, subject_dir,
-                                                                              'episode{}.csv'.format(i+1)),
-                                                                 index_label='Icustay')
+            episodic_data.loc[stay_id, "Weight"] = get_first_valid_from_timeseries(
+                episode, "Weight"
+            )
+            episodic_data.loc[stay_id, "Height"] = get_first_valid_from_timeseries(
+                episode, "Height"
+            )
+        episodic_data.loc[episodic_data.index == stay_id].to_csv(
+            os.path.join(
+                args.subjects_root_path, subject_dir, "episode{}.csv".format(i + 1)
+            ),
+            index_label="Icustay",
+        )
         columns = list(episode.columns)
         columns_sorted = sorted(columns, key=(lambda x: "" if x == "Hours" else x))
         episode = episode[columns_sorted]
-        episode.to_csv(os.path.join(args.subjects_root_path, subject_dir, 'episode{}_timeseries.csv'.format(i+1)),
-                       index_label='Hours')
+        episode.to_csv(
+            os.path.join(
+                args.subjects_root_path,
+                subject_dir,
+                "episode{}_timeseries.csv".format(i + 1),
+            ),
+            index_label="Hours",
+        )
+        return "ok"
+
+
+def to_iterator(obj_ids):
+    while obj_ids:
+        done, obj_ids = ray.wait(obj_ids)
+        yield ray.get(done[0])
+
+
+futures = [
+    extract.remote(subject_dir, args)
+    for subject_dir in os.listdir(args.subjects_root_path)
+]
+
+for x in tqdm(to_iterator(futures), total=len(futures)):
+    pass
+
